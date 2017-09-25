a8008d064e28c34867e7413e04f7894ea5730424
Aaron Samuel <aasamuelj@gmail.com>
2017-09-25 00:59:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,16 @@
 require 'pry'
 def line(arg)
+  counter = 1
+  output = ""
   binding.pry
-  puts "The line is currently empty."
+  if arg.size == 0
+    puts "The line is currently empty."
+  elsif arg.size > 0
+    print "The line is currently: "
+    arg.each do |person|
+      output << "#{counter}. #{person} "
+      counter += 1
+    end
+    output.chomp
+  end
 end
