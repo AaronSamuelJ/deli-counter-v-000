71bc1f987acba84389e41de65c671404849577db
Aaron Samuel <aasamuelj@gmail.com>
2017-09-25 01:01:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,11 +5,10 @@ def line(arg)
   if arg.size == 0
     puts "The line is currently empty."
   elsif arg.size > 0
-    print "The line is currently: "
     arg.each do |person|
       output << "#{counter}. #{person} "
       counter += 1
     end
-    print output.chomp
+    puts "The line is currently: #{output.chomp}"
   end
 end
